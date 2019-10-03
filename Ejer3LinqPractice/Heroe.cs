aeb1d8b79ceba5ae140e0e59a689e8ffcc4fd6b7
Jose <jmmontero.ortega@gmail.com>
2019-10-03 23:26:52 +0200

    Init commit include json data and entity class

---
+++ Ejer3LinqPractice/Heroe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer3LinqPractice
+{
+    public class Heroe
+    {
+        public string Name { get; set; }
+
+        public int Hp { get; set; }
+
+        public int Mp { get; set; }
+
+        public string HeroeClass { get; set; }
+
+        public int Level { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Description { get; set; }
+    }
+}
