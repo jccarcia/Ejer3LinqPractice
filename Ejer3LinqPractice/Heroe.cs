af91914805ca1e6d81125f861aab18bcc989e453
Juan Carlos Cárdenas García <Jccarcia98@gmail.com>
2019-10-09 22:00:49 +0200

    .

--- Ejer3LinqPractice/Heroe.cs
+++ Ejer3LinqPractice/Heroe.cs
@@ -9,17 +9,43 @@ namespace Ejer3LinqPractice
     public class Heroe
     {
         public string Name { get; set; }
-
         public int Hp { get; set; }
-
         public int Mp { get; set; }
-
-        public string HeroeClass { get; set; }
-
+        public string Class { get; set; }
         public int Level { get; set; }
-
         public string Gender { get; set; }
-
         public string Description { get; set; }
+
+        //Constructor con parametros
+        public Heroe(string name, int hp, int mp, string @class, int level, string gender, string description)
+        {
+            Name = name;
+            Hp = hp;
+            Mp = mp;
+            Class = @class;
+            Level = level;
+            Gender = gender;
+            Description = description;
+        }
+
+        //Constructor sin parametros
+        public Heroe()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            String result = "";
+
+            result += "Nombre :" + Name;
+            result += "HP :" + Hp;
+            result += "MP :" + Mp;
+            result += "Clase :" + @Class;
+            result += "Nivel :" + Level;
+            result += "Genero :" + Gender;
+            result += "Descripcion :" + Description;
+        }
     }
 }
+
--- Ejer3LinqPractice/Program.cs
+++ Ejer3LinqPractice/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,25 @@ namespace Ejer3LinqPractice
     {
         static void Main(string[] args)
         {
+            //Abrimos el archivo JSON:
+            string a = File.ReadAllText("Heroes.json");
+
+            //Agregamos el archivo JSON a una Lista para utilizar LINQ:
+            List<Heroe> Hero = JsonConvert.DeserializeObject<List<Heroe>>(a);
+            Console.WriteLine("hola");
+
+            ejer2();
+
+        }
+
+        public static void ejer2(List<Heroe> hero)
+        {
+            var genero = hero.Where(g => g.Gender == "Male");
+            Console.WriteLine("Hombres :");
+            foreach (var x in hero)
+            {
+                Console.WriteLine(x.ToString());
+            }
         }
     }
 }
